876ef5786d07a3d6386d232f1d243b6236fed5af
Alvin Pascual <alvinpascual@superstar.local>
2014-12-17 14:59:49 +1300

    Fixed bug where other users can see someone's uploaded files

--- client/templates/success.js
+++ client/templates/success.js
@@ -1,5 +1,13 @@
 Template.success.helpers({
   topSecret: function () {
+
+  Meteor.subscribe('secret');
+  Meteor.subscribe('files', Meteor.userId());
+
+    if(Secret0.find().count() !== 0){
+      console.log('Pascual');
+      return Secret0.find().fetch();
+    }
     if(Secret1.find().count() !== 0){
       console.log('Shinra');
       return Secret1.find().fetch();
@@ -8,15 +16,6 @@ Template.success.helpers({
       console.log('Cocoon');
       return Secret2.find().fetch();
     }
-    if(Meteor.users.find().count() !== 0){
-      console.log('Pascual');
-      return Meteor.users.find().fetch();
-    }
     return;
   }
 });
-
-Template.success.rendered = function () {
-  Meteor.subscribe('secret');
-  Meteor.subscribe('files', Meteor.userId());
-}
